e75ec20eabb5d857ecc07ba1dbbb72f2ad81cd8b
uosafk84-afk <uosafk84@gmail.com>
2026-08-31 07:39:39 +0430

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'star-shop-v17';
+const CACHE = 'star-shop-v18';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
